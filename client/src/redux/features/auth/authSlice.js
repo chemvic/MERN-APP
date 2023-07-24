54fe0ca80cfd75f2c0eacf829b153db3ce66fb3e
chemvic <chemvic79@gmail.com>
2023-07-24 22:38:59 +0300

    Update authSlice.js

--- client/src/redux/features/auth/authSlice.js
+++ client/src/redux/features/auth/authSlice.js
@@ -1,4 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from '../../../utils/axios';
+
 
 const initialState= {
     user: null,
@@ -6,11 +8,37 @@ const initialState= {
     isLoading: false,
     status: null,
 };
+export const registerUser = createAsyncThunk('auth/registerUser', async (username, password)=>{
+    try {
+        const {data} =await axios.post('/auth/register', {username, password});
+        if (data.token) {
+            window.localStorage('token',data.token);
+        };
+        return data;
+    } catch (error) {
+        
+    }
+})
 
 export const authSlice = createSlice ({
     name: 'auth',
     initialState,
     reducers: {},
+    extraReducers: {
+        [registerUser.pending]: (state)=>{
+            state.isLoading = true;
+            state.status = null;
+        },
+        [registerUser.fulfilled]: (state, action)=>{
+            state.isLoading = false;
+            state.status = action.payload.message;
+            state.user = action.payload.user;
+        },
+        },
+        [registerUser.rejected]: (state, action)=>{
+            state.status = 
+        },
+    }
 });
 
 export const authReducer = authSlice.reducer;
